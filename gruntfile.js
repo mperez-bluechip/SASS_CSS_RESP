1e9bd3a595ed4200a5b06209543f916a2c5137a5
Manny Perez <mannyperez@Mannys-MacBook-Pro.local>
2016-09-30 13:00:55 -0500

    Added dependicies, readme, and packages

---
+++ gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.initConfig({
+      uglify: {
+         my_target: {
+           files: {
+             '_/js/script.js': ['_/components/js/*.js']
+           }//files
+         }//my_target
+      },//uglify
+      watch: {
+          files: ['_/components/js/*.js'],
+          tasks: ['uglify']
+      }//watch
+    });//initConfig
+    grunt.registerTask('default', 'watch');
+}//exports
